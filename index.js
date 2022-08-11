52a675b93910bcea033f20b9d3f711ff640499f1
akort <akortysh@yandex.ru>
2022-08-11 22:39:17 +0500

    Add play again button and comments on the rounds

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ const playOneRound = (playerChoice)=>{
     const playerIndex = possibilities.indexOf(playerChoice)
     , computerIndex = possibilities.indexOf(computerChoice);
 
-    const computerWinMessage = "Computer wins! Know your place, miserable human beeing!" + `${toCamelCase(computerChoice)} beats ${toCamelCase(playerChoice)}`;
-    const playerWinMessage = "You win? How? Impossible...\n" + `${toCamelCase(playerChoice)} beats ${toCamelCase(computerChoice)}`;
+    const computerWinMessage = "Computer wins! Know your place, miserable human beeing!\n" + `${toCamelCase(computerChoice)} beats ${toCamelCase(playerChoice)}.`;
+    const playerWinMessage = "You win? How? Impossible...\n" + `${toCamelCase(playerChoice)} beats ${toCamelCase(computerChoice)}.`;
 
     if (playerChoice == computerChoice) return "You tied a computer! Not bad!";
     if (playerChoice == "paper" && computerChoice == "scissors") return computerWinMessage;
     if (playerChoice == "scissors" && computerChoice == "paper") return playerWinMessage;
     return computerIndex < playerIndex ? computerWinMessage : playerWinMessage; 
+}
+
+const updateScoresDOM = ()=>{
+    let computerScoreParagraph = document.querySelector(".computer p");
+    let playerScoreParagraph = document.querySelector(".player p");
+    computerScoreParagraph.innerText = computerScore;
+    playerScoreParagraph.innerText = playerScore;
 }
 
 let computerScore = 0;
@@ -32,32 +39,57 @@ const updateScores = (playerChoice)=>{
     const roundResult = playOneRound(playerChoice);
     if (roundResult.includes("Computer wins!")) computerScore++;
     if (roundResult.includes("You win?")) playerScore++;
-    let computerScoreParagraph = document.querySelector(".computer p");
-    let playerScoreParagraph = document.querySelector(".player p");
-    computerScoreParagraph.innerText = computerScore;
-    playerScoreParagraph.innerText = playerScore;
+    let commentParagraph = document.querySelector(".comments p");
+    commentParagraph.innerText = roundResult;
+    updateScoresDOM();
     if (computerScore == 5 || playerScore == 5) {
-        rockButton.setAttribute('disabled', 'disabled');
-        paperButton.setAttribute('disabled', 'disabled');
-        scissorsButton.setAttribute('disabled', 'disabled');
+        choiceButtons.forEach((btn)=>{
+            btn.setAttribute('disabled', true);
+        });
         let crownImage;
-        if (playerScore == 5) crownImage = document.querySelector('.player .crown');
-        if (computerScore == 5) crownImage = document.querySelector('.computer .crown');
+        if (playerScore == 5) {
+            crownImage = document.querySelector('.player .crown');
+            commentParagraph.innerText = 'You win! Congratilations!!!';
+        }
+        if (computerScore == 5) {
+            crownImage = document.querySelector('.computer .crown');
+            commentParagraph.innerText = 'It went as expected... Hit that \'Play again\' button and try untill you win!';
+        }
         crownImage.style.visibility = 'visible';
     }
 }
+
+let choiceButtons = [];
 
 const rockButton = document.querySelector(".rock");
 rockButton.addEventListener('click', (e)=>{
     updateScores("rock");
 })
+choiceButtons.push(rockButton);
 
 const paperButton = document.querySelector(".paper");
 paperButton.addEventListener('click', (e)=>{
     updateScores("paper");
 })
+choiceButtons.push(paperButton);
 
 const scissorsButton = document.querySelector(".scissors");
 scissorsButton.addEventListener('click', (e)=>{
     updateScores("scissors");
+})
+choiceButtons.push(scissorsButton);
+
+
+const playAgainButton = document.querySelector(".play-again");
+playAgainButton.addEventListener('click', (e)=>{
+    playerScore = 0;
+    computerScore = 0;
+    updateScoresDOM();
+    let crowns = document.querySelectorAll(".crown");
+    crowns.forEach((crown)=>{
+        crown.style.visibility = 'hidden';
+    });
+    choiceButtons.forEach((btn)=>{
+        btn.disabled = false;
+    });
 })
